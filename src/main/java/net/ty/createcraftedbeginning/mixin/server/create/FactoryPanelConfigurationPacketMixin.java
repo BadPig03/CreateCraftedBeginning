4e51fced1d587965e9c8cb44247337484298becf
fish <fish3www@gmail.com>
2026-08-04 22:21:52 +0800

    fix: 仪表请求气体外物品上限被锁定为 64

--- src/main/java/net/ty/createcraftedbeginning/mixin/server/create/FactoryPanelConfigurationPacketMixin.java
+++ src/main/java/net/ty/createcraftedbeginning/mixin/server/create/FactoryPanelConfigurationPacketMixin.java
@@ -40,8 +40,9 @@ public abstract class FactoryPanelConfigurationPacketMixin {
 
         for (Entry<FactoryPanelPosition, Integer> entry : inputAmounts.entrySet()) {
             FactoryPanelBehaviour source = FactoryPanelBehaviour.at(blockEntity.getLevel(), entry.getKey());
-            boolean gasInput = source != null && GasVirtualUtils.isVirtualItem(source.getFilter());
-            entry.setValue(Mth.clamp(entry.getValue(), 0, gasInput ? GasFactoryGaugeBehaviour.MAX_TARGET_AMOUNT : 64));
+            if (source != null && GasVirtualUtils.isVirtualItem(source.getFilter())) {
+                entry.setValue(Mth.clamp(entry.getValue(), 0, GasFactoryGaugeBehaviour.MAX_TARGET_AMOUNT));
+            }
         }
     }
 
